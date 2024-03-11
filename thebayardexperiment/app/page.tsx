617cbd379f332acc58d560c1e3aaa6c4d9d23fd3
jweaver9 <161187647+jweaver9@users.noreply.github.com>
2024-03-11 16:08:47 +0000

    Round One (2)

--- thebayardexperiment/app/page.tsx
+++ thebayardexperiment/app/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import React from "react";
 import metadata from "metadata"
 
+export {metadata}; // This is the metadata object from thebayardexperiment/app/metadata.tsx
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
